dc34f7851a54b410c7e1fa7b3a5b4dc10e46ecbc
Ataberk Kılavuzcu <ataberk.kilavuzcu@gmail.com>
2023-07-30 15:32:43 +0300

    Goes to Google then searches youtube.

--- src/main/java/ataberkkilavuzcu/App.java
+++ src/main/java/ataberkkilavuzcu/App.java
@@ -1,6 +1,9 @@
 package ataberkkilavuzcu;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -9,6 +12,14 @@ public class App
     public static void main( String[] args )
     {
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.findElement(By.className("gLFyf")).sendKeys("youtube");
+        driver.findElement(By.className("gLFyf")).sendKeys(Keys.ENTER);
+        // driver.quit();
+
 
     }
 }
